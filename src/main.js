5424835af29b7c3324771ad83cc258c03334344c
ehmicky <ehmicky@gmail.com>
2022-08-07 13:41:55 +0200

    Initial implementation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,98 @@
-export default function guessJsonIndent(value) {
-  return value
+// Guess the indentation of a JSON string
+export default function guessJsonIndent(jsonString) {
+  const firstNonWsCharIndex = skipWhitespaces(jsonString, 0)
+
+  if (
+    firstNonWsCharIndex === undefined ||
+    !isJsonObjectOrArray(jsonString[firstNonWsCharIndex])
+  ) {
+    return
+  }
+
+  const secondNonWsCharIndex = skipWhitespaces(
+    jsonString,
+    firstNonWsCharIndex + 1,
+  )
+
+  if (secondNonWsCharIndex === undefined) {
+    return
+  }
+
+  return getIndent(jsonString, firstNonWsCharIndex, secondNonWsCharIndex)
+}
+
+// Whitespaces are ignored before|between|after tokens in JSON
+const skipWhitespaces = function (jsonString, startIndex) {
+  // eslint-disable-next-line fp/no-loops, fp/no-mutation, fp/no-let
+  for (let index = startIndex; index < jsonString.length; index += 1) {
+    const character = jsonString[index]
+
+    // eslint-disable-next-line max-depth
+    if (!isJsonWhitespace(character)) {
+      return index
+    }
+  }
+}
+
+// JSON defines only those are valid whitespaces
+const isJsonWhitespace = function (character) {
+  return (
+    character === ' ' ||
+    character === '\t' ||
+    character === '\n' ||
+    character === '\r'
+  )
+}
+
+// If the top-level value is another type than an object or an array, there is
+// no possible indentation
+const isJsonObjectOrArray = function (character) {
+  return character === '{' || character === '['
+}
+
+// eslint-disable-next-line complexity, max-statements
+const getIndent = function (
+  jsonString,
+  firstNonWsCharIndex,
+  secondNonWsCharIndex,
+) {
+  // eslint-disable-next-line fp/no-let, init-declarations
+  let indent
+
+  // eslint-disable-next-line fp/no-loops
+  for (
+    // eslint-disable-next-line fp/no-let
+    let index = secondNonWsCharIndex - 1;
+    index > firstNonWsCharIndex;
+    // eslint-disable-next-line fp/no-mutation
+    index -= 1
+  ) {
+    const character = jsonString[index]
+
+    // eslint-disable-next-line max-depth
+    if (character === '\r') {
+      return
+    }
+
+    // eslint-disable-next-line max-depth
+    if (character === '\n') {
+      // eslint-disable-next-line max-depth
+      if (indent === undefined) {
+        return 0
+      }
+
+      return indent[0] === ' ' ? indent.length : indent
+    }
+
+    // eslint-disable-next-line max-depth
+    if (indent === undefined) {
+      // eslint-disable-next-line fp/no-mutation
+      indent = character
+    } else if (indent[0] === character) {
+      // eslint-disable-next-line fp/no-mutation
+      indent += character
+    } else {
+      return
+    }
+  }
 }
--- test/main.js
+++ test/main.js
@@ -1,6 +1,41 @@
 import test from 'ava'
 import guessJsonIndent from 'guess-json-indent'
+import { each } from 'test-each'
 
-test('Dummy test', (t) => {
-  t.true(guessJsonIndent(true))
-})
+each(
+  [
+    [''],
+    [' '],
+    ['\n\n'],
+    ...['{', '['].flatMap((firstChar) => [
+      [` ${firstChar}\n "`, 1],
+      [`  ${firstChar}\n "`, 1],
+      [`\n\n${firstChar}\n "`, 1],
+      [`\n   \n${firstChar}\n "`, 1],
+      [`\r\n${firstChar}\n "`, 1],
+      [`\t${firstChar}\n "`, 1],
+      [`\t\t${firstChar}\n "`, 1],
+      [`   \r\n  \t\n  ${firstChar}\n "`, 1],
+      [`${firstChar} \n "`, 1],
+      [`${firstChar}\t\n "`, 1],
+      [`${firstChar}\r\n "`, 1],
+      [`${firstChar}\n\r "`],
+      [`${firstChar}\n  \n\n "`, 1],
+      [firstChar],
+      [`${firstChar}"`],
+      [`${firstChar}\n"`, 0],
+      [`${firstChar}\n "`, 1],
+      [`${firstChar}\n  "`, 2],
+      [`${firstChar}\n\t"`, '\t'],
+      [`${firstChar}\n\t\t"`, '\t\t'],
+      [`${firstChar}\n \t"`],
+      [`${firstChar}\n\t "`],
+    ]),
+  ],
+  (_, [jsonString, indent]) => {
+    const title = JSON.stringify(jsonString).replace(/ /gu, '_')
+    test(`Guesses indent | ${title}`, (t) => {
+      t.is(guessJsonIndent(jsonString), indent)
+    })
+  },
+)
